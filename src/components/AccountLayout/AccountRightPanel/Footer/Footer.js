3984c3078ff77bd1a7201940e6856c3adf735e9d
yagmurcetintas <yagmurcetin@gmail.com>
2019-07-27 14:42:00 -0400

    deleted unnecessary code from AccountRightPanel css

--- src/components/AccountLayout/AccountRightPanel/Footer/Footer.js
+++ src/components/AccountLayout/AccountRightPanel/Footer/Footer.js
@@ -3,11 +3,11 @@ import "./Footer.css";
 
 class Footer extends React.Component {
     render() {
-        return <div className="text-white text-m p-8 flex justify-between items-baseline">
-                <p className="footerLinks m-3"><a href="www.google.com" > Gizlilik Politikası </a></p>
-                <p className="footerLinks m-3"><a href="www.google.com"> Kişisel Verilerin İşlenmesi </a></p>
-                <p className="footerLinks m-3"><a href="www.google.com"> Kişisel Verilerin Korunması </a></p>
-                <p className="footerLinks m-3 font-medium text-xl"><a href="www.google.com"> workhow </a></p>
+        return <div className="text-white text-m px-20 py-8 flex justify-between items-baseline">
+                <p className="inline-block m-3"><a href="www.google.com" > Gizlilik Politikası </a></p>
+                <p className="inline-block m-3"><a href="www.google.com"> Kişisel Verilerin İşlenmesi </a></p>
+                <p className="inline-block m-3"><a href="www.google.com"> Kişisel Verilerin Korunması </a></p>
+                <p className="inline-block m-3 font-medium text-xl"><a href="www.google.com"> workhow </a></p>
             </div>
     }
 }
