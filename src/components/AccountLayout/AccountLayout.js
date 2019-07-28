3a3249df06a2d5ec75b1477464df55fbd9cd8b2b
yagmurcetintas <yagmurcetin@gmail.com>
2019-07-28 06:06:21 -0400

    created ResetPassword component

--- src/components/AccountLayout/AccountLayout.js
+++ src/components/AccountLayout/AccountLayout.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./AccountLayout.css";
 import AccountRightPanel from "./AccountRightPanel/AccountRightPanel"
-import RegisterStepTwo from "./Register/RegisterStepTwo/RegisterStepTwo"
+import ResetPassword from "./ResetPassword/ResetPassword"
 
 class AccountLayout extends React.Component {
     render() {
         return <div>
             <div className="flex flex-col lg:flex-row">
                 <div className="w-full lg:w-2/5 h-screen p-20">
-                    <RegisterStepTwo />
+                    <ResetPassword />
                 </div>
                 <div className="order-first lg:order-last w-full lg:w-3/5 h-screen">
                     <AccountRightPanel />
