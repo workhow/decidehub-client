8ccd09564d59958f237ef0f566d52d0615b2d6c7
Ali Tas <ali.tas@edelkrone.com>
2020-10-25 15:27:26 -0400

    Fixed bug where options were not posted to server

--- src/components/MainLayout/Polls/SharePollModal/SharePollModal.js
+++ src/components/MainLayout/Polls/SharePollModal/SharePollModal.js
@@ -8,11 +8,11 @@ import TextArea from "../../../AccountLayout/Register/TextArea/TextArea";
 class SharePollModal extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       name: "",
       description: "",
-      options: ["", ""]
+      options: ["", ""],
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -34,15 +34,16 @@ class SharePollModal extends Component {
         startSharePollPath,
         {
           name: this.state.title,
-          description: this.state.description
+          description: this.state.description,
+          options: this.state.options,
         },
         { headers: Util.authenticationHeaders() }
       )
-      .then(response => {
+      .then((response) => {
         this.props.toggleDrawer("congratssharemodal", "right", true)(event);
         this.props.refreshData(true);
       })
-      .catch(error => {
+      .catch((error) => {
         if (error.response && error.response.status === 401) {
           Util.signOut();
         } else {
@@ -50,11 +51,11 @@ class SharePollModal extends Component {
         }
       });
   }
-  
+
   addOption() {
     this.setState({
       ...this.state,
-      options: [...this.state.options, ""]
+      options: [...this.state.options, ""],
     });
   }
 
@@ -64,17 +65,17 @@ class SharePollModal extends Component {
 
     this.setState({
       ...this.state,
-      options: this.state.options.filter((val, index) => index !== optionIndex)
+      options: this.state.options.filter((val, index) => index !== optionIndex),
     });
   }
-  
+
   handleInputChange(event) {
     const target = event.target;
     const value = target.value;
     const name = target.name;
     this.setState({ [name]: value });
   }
-  
+
   handleOptionChange(event) {
     const target = event.target;
     const optionVal = target.value;
@@ -83,14 +84,14 @@ class SharePollModal extends Component {
       ...this.state,
       options: this.state.options.map((val, index) =>
         index === optionIndex ? optionVal : val
-      )
+      ),
     });
   }
 
   render() {
     return (
       <div className="flex flex-col px-3 mt-8">
-        <p className="text-2xl mt-4 text-gray-dark">Oylama Ayarları</p>
+        <p className="mt-4 text-2xl text-gray-dark">Oylama Ayarları</p>
         <div className="mt-8">
           <FormBlock
             name="name"
@@ -112,7 +113,7 @@ class SharePollModal extends Component {
           />
         </div>
         {this.state.options.map((val, index) => (
-          <div className="mt-4 flex" key={index}>
+          <div className="flex mt-4" key={index}>
             <FormBlock
               className="flex-grow text-base text-gray-dark"
               labelText={`${index + 1}. Seçenek`}
@@ -124,17 +125,19 @@ class SharePollModal extends Component {
 
             {index > 1 && (
               <button
-                className="bg-red-500 text-white font-bold uppercase py-3 px-4 text-xs self-end "
+                className="self-end px-4 py-3 text-xs font-bold text-white uppercase bg-red-500 "
                 onClick={this.removeOption}
-                data-index={index}>
+                data-index={index}
+              >
                 Sil
               </button>
             )}
           </div>
         ))}
         <button
-          className="bg-green-500 text-white font-bold uppercase py-3 px-4 mt-4 mb-12 text-xs w-full"
-          onClick={this.addOption}>
+          className="w-full px-4 py-3 mt-4 mb-12 text-xs font-bold text-white uppercase bg-green-500"
+          onClick={this.addOption}
+        >
           Seçenek Ekle
         </button>
 
